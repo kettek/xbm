10cfde2b71c0c26e28f7553c9b37417d5adbafa7
kts of kettek (yan) <kts@kettek.net>
2021-02-25 04:56:59 -0800

    Add working decode test

--- reader_test.go
+++ reader_test.go
@@ -1,28 +1,46 @@
 package xbm
 
 import (
+	"fmt"
+	"image"
 	"os"
 	"testing"
 )
 
 func TestReadAndPrint(t *testing.T) {
-	_, err := ReadXBM("tests/goblin.xbm")
+	m, err := DecodeXBM("tests/goblin.xbm")
 	if err != nil {
 		t.Error(err)
 	}
+	b := m.Bounds()
+	fmt.Printf("Decoded XBM Dimensions: %dx%d->%dx%d\n", b.Min.X, b.Min.Y, b.Max.X, b.Max.Y)
+	for y := b.Min.Y; y < b.Max.Y; y++ {
+		for x := b.Min.X; x < b.Max.X; x++ {
+			v, ok := m.At(x, y).(BitColor)
+			if !ok {
+				continue
+			}
+			if v {
+				fmt.Printf(".")
+			} else {
+				fmt.Printf(" ")
+			}
+		}
+		fmt.Println("")
+	}
 }
 
-func ReadXBM(path string) (XBM, error) {
+func DecodeXBM(path string) (image.Image, error) {
 	f, err := os.Open(path)
 	if err != nil {
-		return XBM{}, err
+		return nil, err
 	}
 	defer f.Close()
 
-	_, err = Decode(f)
+	img, err := Decode(f)
 	if err != nil {
-		return XBM{}, err
+		return nil, err
 	}
 
-	return XBM{}, err
+	return img, err
 }
