b2582e86401cf08e802b3fc19f667361242478da
kts of kettek (POWERQWACK) <kts@kettek.net>
2021-02-25 05:39:03 -0800

    Add documentation

--- image.go
+++ image.go
@@ -5,6 +5,7 @@ import (
 	"image/color"
 )
 
+// Bits is an in-memory image whose At method returns color.BitColor values.
 type Bits struct {
 	// Pix holds the image's pixels, as boolean values.
 	Pix []bool
@@ -14,10 +15,13 @@ type Bits struct {
 	Rect image.Rectangle
 }
 
+// ColorModel returns BitColorModel.
 func (b *Bits) ColorModel() color.Model { return BitColorModel }
 
+// Bounds returns the image's bounds.
 func (b *Bits) Bounds() image.Rectangle { return b.Rect }
 
+// At returns the pixel color at (x, y).
 func (b *Bits) At(x, y int) color.Color {
 	if !(image.Point{x, y}.In(b.Rect)) {
 		return BitColor(false)
@@ -26,10 +30,14 @@ func (b *Bits) At(x, y int) color.Color {
 	return BitColor(b.Pix[i])
 }
 
+// PixOffset returns the index of the first element of Pix that corresponds to
+// the pixel at (x, y).
 func (b *Bits) PixOffset(x, y int) int {
 	return (y-b.Rect.Min.Y)*b.Stride + (x - b.Rect.Min.X)
 }
 
+// Set sets the binary pixel value at (x, y) to match the provided color,
+// using BitColorModel's conversion algorithm.
 func (b *Bits) Set(x, y int, c color.Color) {
 	if !(image.Point{x, y}.In(b.Rect)) {
 		return
@@ -38,6 +46,7 @@ func (b *Bits) Set(x, y int, c color.Color) {
 	b.Pix[i] = bool(BitColorModel.Convert(c).(BitColor))
 }
 
+// SetBit sets the binary pixel value at (x, y) to a boolean value.
 func (b *Bits) SetBit(x, y int, c bool) {
 	if !(image.Point{x, y}.In(b.Rect)) {
 		return
@@ -46,6 +55,8 @@ func (b *Bits) SetBit(x, y int, c bool) {
 	b.Pix[i] = c
 }
 
+// SubImage returns an image representing the portion of the image p visible
+// through r. The returned value shares pixels with the original image.
 func (b *Bits) SubImage(r image.Rectangle) image.Image {
 	r = r.Intersect(b.Rect)
 	if r.Empty() {
@@ -58,10 +69,13 @@ func (b *Bits) SubImage(r image.Rectangle) image.Image {
 	}
 }
 
+// Opaque scans the entire image and reports whether it is fully opaque.
+// This is always true for XBM images.
 func (b *Bits) Opaque() bool {
 	return true
 }
 
+// NewBits returns a new Bits image with the given bounds.
 func NewBits(r image.Rectangle) *Bits {
 	return &Bits{
 		Pix:    make([]bool, r.Max.Y*r.Max.X+r.Max.X),
